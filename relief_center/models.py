dd585b1a2cf3906edb52eaf5cf7e7cb1bbcb5041
Mahesh Chandran <maheshchandran147@gmail.com>
2018-11-10 04:01:56 +0530

    Added relief_center model and did migrations

--- relief_center/models.py
+++ relief_center/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class ReliefCenter(models.Model):
+    contact_no = models.IntegerField()
+    location = models.TextField()
+    donation_received = models.IntegerField()
+    type_of_disaster = models.CharField(max_length=100)
