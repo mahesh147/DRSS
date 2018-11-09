5a05af37dba642d4ae54eb417e5e36de5e426f5a
Mahesh Chandran <maheshchandran147@gmail.com>
2018-11-10 00:37:10 +0530

    Added MySQL server connections to settings.py

--- DRSS/settings.py
+++ DRSS/settings.py
@@ -69,14 +69,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'DRSS.wsgi.application'
 
-
+os.path.join(BASE_DIR, 'db.sqlite3'),
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, '.mylogin.cnf'),
+        },
     }
 }
 
